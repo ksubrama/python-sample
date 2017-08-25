ed487238c89fe2ffb05264ccbb3f14625c9aadda
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2017-08-25 04:03:33 -0400

    Use sys to exit app with return code
    Signed-off-by: Kartik Null Cating-Subramanian <ksubramanian@chef.io>

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import sys
 from sample import core
 
 
 def main():
   s = core.SampleCore('a thing')
   print('Thing: %s' % s.thing)
+  return 0
 
 
 if __name__ == "__main__":
-    main()
+  sys.exit(main())
